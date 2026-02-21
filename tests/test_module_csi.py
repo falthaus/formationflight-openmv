49e5071fac1563e790e78a6129cf2083db1b3526
falthaus <althaus.felix@gmail.com>
2026-02-21 09:37:08 +0100

    Various minor updates

--- tests/test_module_csi.py
+++ tests/test_module_csi.py
@@ -1,13 +1,17 @@
 """
-Testing basic features and behaviour with new 'csi' module for GRAYSCALE and RGB565
-The 'sensor' module is deprecated
-Also using the 'time' module, instead of the deprecated 'pyb' module
+Testing basic features and behaviour with new 'csi' module instead of the now
+deprecated 'sensor' module.
+Also using the 'time' module, instead of the deprecated 'pyb' module.
+
+Constraint for the OpenMV H7: using grayscale VGA or WVGA2 without cropping (CROP_FACTOR=1)
+is only supported wth custom firmware, otherwise there isn't sufficient memory available.
+This custom firmware does not support RGB565 at any resolution.
 
 (C) 2026  Felix Althaus
 
 """
 
-CROP_FACTOR = 0.8   # 1.0 or lower if full frame size is not supported
+CROP_FACTOR = 1.0   # 1.0 or lower if full frame size is not supported
 
 
 import csi
@@ -17,10 +21,10 @@ import time
 sensor = csi.CSI()
 sensor.reset()
 sensor.pixformat(csi.GRAYSCALE)
-sensor.framesize(csi.VGA)
+sensor.framesize(csi.WVGA2)
 sensor.window((int(sensor.width()*CROP_FACTOR),
                int(sensor.height()*CROP_FACTOR)))
-#sensor.auto_exposure(False, exposure_us=8000)
+#sensor.auto_exposure(False, exposure_us=2000)
 sensor.auto_gain(False, gain_db=0)
 
 
@@ -34,13 +38,13 @@ while True:
     img = sensor.snapshot()
     t = time.ticks_us()
 
-    blobs = img.find_blobs([(255, 255)], merge=False)
+    blobs = img.find_blobs([(255, 255)], x_stride=2, y_stride=2, merge=False)
 
     for blob in blobs:
         img.draw_rectangle(blob.rect(), color=0)
         img.draw_cross(blob.cx(), blob.cy(), color=0)
 
-    t_exp = sensor.exposure_us()
+    exposure_us = sensor.exposure_us()
 
     # Frame ID:
     img.draw_rectangle(0, 6-6, 2+(7*8)+2, 2+(1*10)+2, color=0, fill=True)
@@ -50,8 +54,7 @@ while True:
     # FPS
     img.draw_string(width-(9*8)-2, 2, f"{1e6/time.ticks_diff(t, t_old):5.1f} FPS")
     # Exposure [us]
-    img.draw_string(width-(9*8)-2, 2+(1*10)+2, f"{t_exp:6d} us")
-
+    img.draw_string(width-(9*8)-2, 2+(1*10)+2, f"{exposure_us:6d} us")
 
     t_old = t
     frame_id = (frame_id + 1) % (2**16)
--- tests/test_module_sensor.py
+++ tests/test_module_sensor.py
@@ -1,24 +1,28 @@
 """
-Testing basic features and behaviour with old 'sensor' module for GRAYSCALE and RGB565
-The 'sensor' module is deprecated
-The 'pyb' module is deprecated
+Testing basic features and behaviour with old 'sensor' module.
+Both the 'sensor' and the 'pyb' modules are deprecated.
+
+Constraint for the OpenMV H7: using grayscale VGA or WVGA2 without cropping (CROP_FACTOR=1)
+is only supported wth custom firmware, otherwise there isn't sufficient memory available.
+This custom firmware does not support RGB565 at any resolution.
 
 (C) 2026  Felix Althaus
 
 """
 
-CROP_FACTOR = 0.8   # 1.0 or lower if full frame size is not supported
+CROP_FACTOR = 1.0   # 1.0 or lower if full frame size is not supported
 
 
 import sensor
 import pyb
 
+
 sensor.reset()
 sensor.set_pixformat(sensor.GRAYSCALE)
-sensor.set_framesize(sensor.VGA)
+sensor.set_framesize(sensor.WVGA2)
 sensor.set_windowing((int(sensor.width()*CROP_FACTOR),
                       int(sensor.height()*CROP_FACTOR)))
-#sensor.set_auto_exposure(False, exposure_us=8000)
+#sensor.set_auto_exposure(False, exposure_us=2000)
 sensor.set_auto_gain(False, gain_db=0)
 
 
@@ -32,16 +36,15 @@ t_old = 0
 while True:
 
     img = sensor.snapshot()
-    t_old = t
     t = pyb.elapsed_micros(t_start)
 
-    blobs = img.find_blobs([(255, 255)], merge=False)
+    blobs = img.find_blobs([(255, 255)], x_stride=2, y_stride=2, merge=False)
 
     for blob in blobs:
         img.draw_rectangle(blob.rect(), color=0)
         img.draw_cross(blob.cx(), blob.cy(), color=0)
 
-    t_exp = sensor.get_exposure_us()
+    exposure_us = sensor.get_exposure_us()
 
     # Frame ID:
     img.draw_rectangle(0, 6-6, 2+(7*8)+2, 2+(1*10)+2, color=0, fill=True)
@@ -51,8 +54,7 @@ while True:
     # FPS
     img.draw_string(width-(9*8)-2, 2, f"{1e6/(t - t_old):5.1f} FPS")
     # Exposure [us]
-    img.draw_string(width-(9*8)-2, 2+(1*10)+2, f"{t_exp:6d} us")
-
+    img.draw_string(width-(9*8)-2, 2+(1*10)+2, f"{exposure_us:6d} us")
 
     t_old = t
     frame_id = (frame_id + 1) % (2**16)
